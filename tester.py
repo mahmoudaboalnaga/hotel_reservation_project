60303e098121e52d2655672476c33c1050fd3f4b
mahmoud <mahmoud.aboalnaga@hotmail.com>
2018-06-09 13:35:14 +0200

    remove mobile number in tester file

--- tester.py
+++ tester.py
@@ -2,7 +2,7 @@ class Tester(object):
     """docstring for Tester."""
 
     hotels=[[20,"Rotana","Abu Dhabi",200,40],[21,"Sheraton","Abu Dhabi",300,100],[22,"Winter balace","Luxor",100,50]]
-    reservation=[["Rotana","Ahmed mohamed","+201060333567"],["Rotana","youssef mohamed","+1000000"],["Winter balace","mohamed moustafa","+1000000"]]
+    reservation=[["Rotana","Ahmed mohamed","+2100000000"],["Rotana","youssef mohamed","+1000000"],["Winter balace","mohamed moustafa","+1000000"]]
     account_sid="" #enter your twilio SID
     auth_token  = "" #enter your twilio auth_token
     twilio_mobile=""#enter your twilio mobile_number
